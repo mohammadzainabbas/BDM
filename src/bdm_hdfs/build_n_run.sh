af46b9b3ac045bb8e8ad639406c94447419462e0
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-04-02 14:54:45 +0000

    Minor fix

--- src/bdm_hdfs/build_n_run.sh
+++ src/bdm_hdfs/build_n_run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-/home/bdm/apache-maven-3.8.5/bin/mvn package && java -cp /home/bdm/BDM/src/bdm_hdfs/target/bdm_hdfs-1.0-SNAPSHOT.jar com.upc.bdm_hdfs.App $@
+/home/bdm/apache-maven-3.8.5/bin/mvn package && java -cp /home/bdm/BDM/src/bdm_hdfs/target/bdm_hdfs-1.0.0.jar com.upc.bdm_hdfs.App $@
 
 # sh build_n_run.sh -write -format parquet -src ~/BDM/src -desc /user/bdm/ -hello
