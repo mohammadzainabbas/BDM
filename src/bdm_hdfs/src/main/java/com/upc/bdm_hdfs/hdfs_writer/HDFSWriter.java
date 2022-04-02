60eed99013e51889a2031d740b0b854472fae4f3
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-04-02 14:27:58 +0000

    Minor update

--- src/bdm_hdfs/src/main/java/com/upc/bdm_hdfs/hdfs_writer/HDFSWriter.java
+++ src/bdm_hdfs/src/main/java/com/upc/bdm_hdfs/hdfs_writer/HDFSWriter.java
@@ -3,10 +3,12 @@ package com.upc.bdm_hdfs.hdfs_writer;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.avro.generic.GenericRecord;
+
 public interface HDFSWriter {
     public void open(String file, File schemaFile) throws IOException;
 	
-	public void put(Object obj);
+	public void put(GenericRecord obj);
 	
 	public void reset();
 	
