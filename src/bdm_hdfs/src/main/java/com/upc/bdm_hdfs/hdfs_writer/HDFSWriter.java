9c5051f59b6b4b07adf911bd0f81366d2b96203a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-04-02 14:06:07 +0000

    Work in progress

--- src/bdm_hdfs/src/main/java/com/upc/bdm_hdfs/hdfs_writer/HDFSWriter.java
+++ src/bdm_hdfs/src/main/java/com/upc/bdm_hdfs/hdfs_writer/HDFSWriter.java
@@ -1,9 +1,10 @@
 package com.upc.bdm_hdfs.hdfs_writer;
 
+import java.io.File;
 import java.io.IOException;
 
 public interface HDFSWriter {
-    public void open(String file) throws IOException;
+    public void open(String file, File schemaFile) throws IOException;
 	
 	public void put(Object obj);
 	
