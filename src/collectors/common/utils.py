c0b0d1403a212494322ffdce99838cbc0057a725
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-04-04 18:43:55 +0000

    Added all common utils for collectors

---
+++ src/collectors/common/utils.py
@@ -0,0 +1,47 @@
+from os import makedirs
+from os.path import exists
+from datetime import datetime
+import requests
+import json
+import csv
+
+def print_log(text):
+    """
+    Logger
+    """
+    print("[ log ] {}".format(text))
+
+def json_to_csv(records, path):
+    """
+    Convert json and save as csv
+    """
+    keys = records[0].keys()
+    with open(path, 'w') as outfile:
+        writer = csv.DictWriter(outfile, keys)
+        writer.writeheader()
+        writer.writerows(records)
+
+def fetch_data(url, verbose=False, raw=False, **kwargs):
+    """
+    Send a GET request to a given url
+    """
+    try:
+
+        if verbose: print_log("Fetching data from: {}".format(url))
+        result = requests.get(url, **kwargs)
+        if raw: return result
+        return json.loads(result.text)
+
+    except Exception as e: print("[Error] {}".format(e))
+
+def get_today_date(format='%Y%m%d'):
+    """
+    Get today's date (in formatted manner)
+    """
+    return datetime.today().strftime(format)
+
+def create_if_not_exists(path):
+    """
+    Create dir if not exists
+    """
+    if not exists(path): makedirs(path)
