7dcf5ccfd5620f2ad9f2fa6892de20b539dfa8db
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-04-04 18:44:48 +0000

    Work in progress for collectors

---
+++ src/collectors/__init__.py
@@ -0,0 +1,9 @@
+# from event import *
+# from common import *
+# from transport import *
+
+from . import event
+from . import common
+from . import transport
+
+__all__ = ['event', 'common', 'transport']
