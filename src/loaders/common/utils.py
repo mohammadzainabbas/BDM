7caf76263e85767a1c7e223a3d31e37396828c62
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-04-04 20:00:55 +0000

    Added utils

--- src/loaders/common/utils.py
+++ src/loaders/common/utils.py
@@ -4,6 +4,42 @@ from datetime import datetime
 import requests
 import json
 import csv
+from constants import HDFS_ADDRESS, HDFS_HOME, HDFS_USER
+from hdfs import InsecureClient
+from hdfs.ext.avro import AvroWriter
+from hdfs.ext.avro import AvroReader
+import glob
+import re
+import shutil
+
+def get_hdfs_address():
+    return HDFS_ADDRESS
+
+def get_hdfs_user():
+    return HDFS_USER
+
+def get_hdfs_user_home():
+    return HDFS_HOME
+
+# client = InsecureClient('http://10.4.41.44:9870', user='bdm')
+
+def mkdirs_hdfs(dir_path):
+    with InsecureClient(url=get_hdfs_address(), user=get_hdfs_user()) as client:
+        client.makedirs(dir_path)
+        return True
+
+def save_as_avro_hdfs(file_path, parsed_data, schema):
+    with InsecureClient(url=get_hdfs_address(), user=get_hdfs_user()) as client:
+        with AvroWriter(client, file_path,schema = schema) as writer:
+            for record in parsed_data:
+                writer.write(record)
+            return True
+
+def json_to_hdfs(file_path, json_object):
+    with InsecureClient(url=get_hdfs_address(), user=get_hdfs_user()) as client:
+        with client.write("{}{}".format(get_hdfs_user_home(), file_path), encoding="utf-8",permission=777, overwrite=True) as writer:
+            json.dump(json_object, writer, ensure_ascii=False)
+            return True
 
 def print_log(text):
     """
