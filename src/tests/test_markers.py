6ad36ab8cee0b2bbaf611e87f4789bc835428035
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-03 14:09:26 +0000

    Minor work

--- src/tests/test_markers.py
+++ src/tests/test_markers.py
@@ -8,81 +8,33 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    start_coords = (-15.79340,-47.88232)
-    folium_map = folium.Map(location=start_coords, zoom_start=16)
+    # start_coords = (-15.79340,-47.88232)
+    # start_coords = (41.3927755,2.0701489)
+    start_coords, zoom_start = (41.389, 2.15), 14
+    folium_map = folium.Map(location=start_coords, zoom_start=zoom_start)
     draw = Draw()
     draw.add_to(folium_map)
     folium_map.add_child(MeasureControl())
     tooltip = 'Click me!'
-    folium.Marker(
-    [-15.796218,-47.872002 ], 
-    popup='<i>Estrada Parque Ceilândia, SIA</i>', 
-    icon=folium.Icon(color='black',icon='car', prefix='fa'),
-    tooltip=tooltip).add_to(folium_map)
-    folium.Marker(
-    [-15.791077,-47.875543], 
-    popup='<b>QSB 12, Taguatinga, Região</b>', 
-    icon=folium.Icon(color='black',icon='car', prefix='fa'),
-    tooltip=tooltip).add_to(folium_map)
-    folium.Marker(
-    [-15.787711,-47.878397], 
-    popup='<b>Bloco A / B, W1 Sul, SQS 104, Asa Sul</b>', 
-    icon=folium.Icon(color='black',icon='car', prefix='fa'),
-    tooltip=tooltip).add_to(folium_map)
-    folium.Marker(
-    [-15.790932,-47.879212], 
-    popup='<b>Recanto das Emas, Região Integrada de Desenvol...</b>', 
-    icon=folium.Icon(color='black',icon='car', prefix='fa'),
-    tooltip=tooltip).add_to(folium_map)
-    folium.Marker(
-    [-15.793678,-47.880199], 
-    popup='<b>Recanto das Emas, Região Integrada de Desenvol...</b>', 
-    icon=folium.Icon(color='black',icon='car', prefix='fa'),
-    tooltip=tooltip).add_to(folium_map)
-    folium.Marker(
-    [-15.79628,-47.885885], 
-    popup='<b>Recanto das Emas, Região Integrada de Desenvol...</b>', 
-    icon=folium.Icon(color='black',icon='car', prefix='fa'),
-    tooltip=tooltip).add_to(folium_map)
-    folium.Marker(
-    [-15.789507,-47.883203], 
-    popup='<b>Recanto das Emas, Região Integrada de Desenvol...</b>', 
-    icon=folium.Icon(color='black',icon='car', prefix='fa'),
-    tooltip=tooltip).add_to(folium_map)
-    folium.Marker(
-    [-47.887988,-47.887988], 
-    popup='<b>Recanto das Emas, Região Integrada de Desenvol...</b>', 
-    icon=folium.Icon(color='black',icon='car', prefix='fa'),
-    tooltip=tooltip).add_to(folium_map)
-    folium.Marker(
-    [-15.79436,-47.887602], 
-    popup='<b>Recanto das Emas, Região Integrada de Desenvol...</b>', 
-    icon=folium.Icon(color='black',icon='car', prefix='fa'),
-    tooltip=tooltip).add_to(folium_map)
-    folium.Marker(
-    [-15.788743,-47.889705], 
-    popup='<b>Recanto das Emas, Região Integrada de Desenvol...</b>', 
-    icon=folium.Icon(color='black',icon='car', prefix='fa'),
-    tooltip=tooltip).add_to(folium_map)
-    folium.Marker(
-    [-15.788826,-47.89346], 
-    popup='<b>Recanto das Emas, Região Integrada de Desenvol...</b>', 
-    icon=folium.Icon(color='black',icon='car', prefix='fa'),
-    tooltip=tooltip).add_to(folium_map)
+
+    folium.Marker([41.376204, 2.1198613], popup='<i>Barcelona Accomodation</i>', icon=folium.Icon(color='black',icon='home', prefix='fa'), tooltip=tooltip).add_to(folium_map)
+    folium.Marker([41.3751105,2.1027038], popup='<i>Kainaat Place</i>', icon=folium.Icon(color='black',icon='home', prefix='fa'), tooltip=tooltip).add_to(folium_map)
+    folium.Marker([41.3873154,2.1112131], popup='<i>Tejaswini Place</i>', icon=folium.Icon(color='black',icon='home', prefix='fa'), tooltip=tooltip).add_to(folium_map)
+    
     formatter = "function(num) {return L.Util.formatNum(num, 3) + ' º ';};"
     MousePosition(
-    position='topright',
-    separator=' | ',
-    empty_string='NaN',
-    lng_first=True,
-    num_digits=20,
-    prefix='Coordinates:',
-    lat_formatter=formatter,
-    lng_formatter=formatter,
+        position='topright',
+        separator=' | ',
+        empty_string='NaN',
+        lng_first=True,
+        num_digits=20,
+        prefix='Coordinates:',
+        lat_formatter=formatter,
+        lng_formatter=formatter,
     ).add_to(folium_map)
+    
     return folium_map._repr_html_()
 
 
