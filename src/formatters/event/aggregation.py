6aa7fde08c4aabef8ec4a1fff2e4e8847e25cb17
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-05 21:27:46 +0000

    Minor

--- src/formatters/event/aggregation.py
+++ src/formatters/event/aggregation.py
@@ -8,10 +8,10 @@ def main():
     files, prefix = get_files(activity_type)
     print_log("Loading {} files of '{}' into HDFS".format(len(files), activity_type))
     hdfs_path = "{}/{}/{}".format(get_hdfs_user_home(), prefix, activity_type)
-    for file in files:
-        file_name = str(file).split("/")[-1]
-        print_log("File '{}' moved to HDFS at '{}'".format(file, hdfs_path))
-        write_to_hdfs(join(hdfs_path, file_name), file)
+    # for file in files:
+    #     file_name = str(file).split("/")[-1]
+    #     print_log("File '{}' moved to HDFS at '{}'".format(file, hdfs_path))
+    #     write_to_hdfs(join(hdfs_path, file_name), file)
     print_log("Saved {} file(s) of '{}' into HDFS at '{}'".format(len(files), activity_type, hdfs_path))
 
 if __name__ == '__main__':
