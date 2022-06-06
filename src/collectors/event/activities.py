5aba2de453b672609072a2582d070d1da4af39ac
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-06 15:05:29 +0000

    Minor update

--- src/collectors/event/activities.py
+++ src/collectors/event/activities.py
@@ -1,14 +1,15 @@
+from dataclasses import dataclass
 from functools import total_ordering
 from os.path import join
 from utils import get_today_date, get_parent, fetch_data, json_to_csv, print_log
 
-BASE_URL = "https://opendata-ajuntament.barcelona.cat/data"
-START_URL = "/api/action/datastore_search?resource_id=877ccf66-9106-4ae2-be51-95a9f6469e4c"
 
 def parse_schema(fields):
     return fields
 
 def get_activities():
+    BASE_URL = "https://opendata-ajuntament.barcelona.cat/data"
+    START_URL = "/api/action/datastore_search?resource_id=877ccf66-9106-4ae2-be51-95a9f6469e4c"
     data = list()
     schema = list()
     is_first = True
@@ -28,15 +29,19 @@ def get_activities():
     return data, schema
 
 def main():
-    url = "https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=877ccf66-9106-4ae2-be51-95a9f6469e4c"
-    activity_type = "activities"
+    # url = "https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=877ccf66-9106-4ae2-be51-95a9f6469e4c"
+    # activity_type = "activities"
     today_date = get_today_date()
-    path = get_parent(join(today_date, activity_type))
-    data = fetch_data(url, verbose=True)
-    records = data['result']['records']
-    file_path = join(path, '{}_{}.csv'.format(activity_type, today_date))
-    json_to_csv(records, file_path)
-    print_log("Fetched {} records on {} for '{}' from url '{}' and saved to '{}'".format(len(records), today_date, activity_type, url, file_path))
+    # path = get_parent(join(today_date, activity_type))
+    # data = fetch_data(url, verbose=True)
+    # records = data['result']['records']
+    # file_path = join(path, '{}_{}.csv'.format(activity_type, today_date))
+    # json_to_csv(records, file_path)
+    # print_log("Fetched {} records on {} for '{}' from url '{}' and saved to '{}'".format(len(records), today_date, activity_type, url, file_path))
+
+    data, schema = get_activities()
+
+    print("Total data: {}".format(len(data)))
 
 if __name__ == '__main__':
     main()
