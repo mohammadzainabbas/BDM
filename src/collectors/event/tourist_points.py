a10446f431d8c2bf539c4956885fb38998a38ddd
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-06 16:18:37 +0000

    Minor update

--- src/collectors/event/tourist_points.py
+++ src/collectors/event/tourist_points.py
@@ -19,16 +19,14 @@ def get_touristic_points() -> list:
         start_url = _result_['result']['_links']['next']
     return data
 
-
 def main():
     activity_type = "tourist_points"
     today_date = get_today_date()
     path = get_parent(join(today_date, activity_type))
-    data = fetch_data(url, verbose=True)
-    records = data['result']['records']
+    data = get_touristic_points()
     file_path = join(path, '{}_{}.csv'.format(activity_type, today_date))
-    json_to_csv(records, file_path)
-    print_log("Fetched {} records on {} for '{}' from url '{}' and saved to '{}'".format(len(records), today_date, activity_type, url, file_path))
+    json_to_csv(data, file_path)
+    print_log("Fetched {} records on {} for '{}' from base url '{}' and saved to '{}'".format(len(data), today_date, activity_type, BASE_URL, file_path))
 
 if __name__ == '__main__':
     main()
