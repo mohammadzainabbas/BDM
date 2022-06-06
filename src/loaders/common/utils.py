3aaf4a81bb2d9372732c608db69be9cd2c3c9cc4
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-06 15:42:03 +0000

    Minor

--- src/loaders/common/utils.py
+++ src/loaders/common/utils.py
@@ -59,9 +59,9 @@ def save_df_as_parquet(file_path: str, df: pd.DataFrame) -> None:
 
 def csv_to_parquet(file_path: str) -> str:
     _file_path = file_path.split('/')
-    path, file_name = str("/".join(_file_path[:-1])), _file_path[-1]
+    path, file_name = str("/".join(_file_path[:-1])), "".join(_file_path[-1].split(".")[:-1])
+    pq.write_table( pcsv.read_csv(file_path) , '{}/{}.parquet'.format(path, file_name))
     
-    pq.write_table( pcsv.read_csv(file_path) , '{}.parquet'.format(path))
 
     
 
