cff71a64ef90f0d5f487d2646c23590a071abbac
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-06 16:11:29 +0000

    Updated code

--- src/collectors/event/activities.py
+++ src/collectors/event/activities.py
@@ -5,19 +5,13 @@ BASE_URL = "https://opendata-ajuntament.barcelona.cat/data"
 
 def get_activities():
     start_url = "/api/action/datastore_search?resource_id=877ccf66-9106-4ae2-be51-95a9f6469e4c"
-    data = list()
-    schema = list()
-    is_first = True
-    total_data = 0
+    data, is_first, total_data = list(), True, 0
     while(True):
         _result_ = fetch_data("{}{}".format(BASE_URL, start_url), verbose=True)
         _data_ = _result_['result']['records']
         data.extend(_result_['result']['records'])
         if is_first:
             total_data = _result_['result']['total']
-            schema = parse_schema(_result_['result']['fields'])
-            print(schema)
-            break
             is_first = False
         total_data = total_data - len(_data_)
         if total_data == 0:
