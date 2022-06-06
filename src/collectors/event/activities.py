0f7ab546052adfe76673f4183894419798effab2
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-06 16:12:30 +0000

    Minor update

--- src/collectors/event/activities.py
+++ src/collectors/event/activities.py
@@ -6,6 +6,7 @@ BASE_URL = "https://opendata-ajuntament.barcelona.cat/data"
 def get_activities():
     start_url = "/api/action/datastore_search?resource_id=877ccf66-9106-4ae2-be51-95a9f6469e4c"
     data, is_first, total_data = list(), True, 0
+    # Fetch till we have all the records (a parameter 'total' in the API call)
     while(True):
         _result_ = fetch_data("{}{}".format(BASE_URL, start_url), verbose=True)
         _data_ = _result_['result']['records']
@@ -14,16 +15,15 @@ def get_activities():
             total_data = _result_['result']['total']
             is_first = False
         total_data = total_data - len(_data_)
-        if total_data == 0:
-            break
+        if total_data == 0: break
         start_url = _result_['result']['_links']['next']
-    return data, schema
+    return data
 
 def main():
     activity_type = "activities"
     today_date = get_today_date()
     path = get_parent(join(today_date, activity_type))
-    data, schema = get_activities()
+    data = get_activities()
     # file_path = join(path, '{}_{}.csv'.format(activity_type, today_date))
     # json_to_csv(data, file_path)
     # print_log("Fetched {} records on {} for '{}' from base url '{}' and saved to '{}'".format(len(data), today_date, activity_type, BASE_URL, file_path))
