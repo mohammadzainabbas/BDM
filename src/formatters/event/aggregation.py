fa21385e884cc9a90f6f5053a887e2267cdcfcd0
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-06 14:39:03 +0000

    Minor changes

--- src/formatters/event/aggregation.py
+++ src/formatters/event/aggregation.py
@@ -3,8 +3,10 @@ from pyspark.sql import SparkSession, SQLContext, functions as SF
 from pyspark import SparkContext, SQLContext
 from utils import get_hdfs_user_home, get_files, write_to_hdfs, print_log
 
-activities_cols = ['register_id', 'name', 'geo_epgs_4326_x', 'geo_epgs_4326_y'
-    'addresses_neighborhood_id', 'addresses_neighborhood_name', 'addresses_district_id', 'addresses_district_name', 'addresses_road_name'
+activities_cols = ['register_id', 'name', 'geo_epgs_4326_x', 'geo_epgs_4326_y', # Must
+    'addresses_neighborhood_id', 'addresses_neighborhood_name', 
+    'addresses_district_id', 'addresses_district_name', 
+    'addresses_road_name', 'addresses_road_id'
 ]
 
 def main():
