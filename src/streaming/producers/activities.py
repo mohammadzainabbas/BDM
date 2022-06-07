407c0c7c95d2995d6546c7e8cf110b6c545b6b13
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-07 14:01:45 +0000

    Minor

--- src/streaming/producers/activities.py
+++ src/streaming/producers/activities.py
@@ -1,8 +1,8 @@
 from kafka import KafkaProducer
-
+from ...collectors.event.activities import get_activities
 
 def main():
-    
+    print(get_activities())
     print("GH")
 
 if __name__ == "__main__":
