28dbc02ac1830abd8ee98e2754285b8b260e5409
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-07 16:53:39 +0000

    Minor changes

--- src/streaming/consumers/activities.py
+++ src/streaming/consumers/activities.py
@@ -4,6 +4,8 @@ from utils import get_today_date, get_parent, fetch_data, print_log, get_kafka_c
 from collections import defaultdict
 from kafka import KafkaConsumer
 
+def get_ac
+
 def get_hello():
     # test_data = { 'name': 'Mohammad', 'age': 27, 'x': -5.12, 'y': 34.48 }
     # print(test_data)
