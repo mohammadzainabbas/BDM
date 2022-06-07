70ade9848d385f663892fba8f02959f76d6bdd16
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-07 14:02:47 +0000

    Minor

--- src/streaming/producers/activities.py
+++ src/streaming/producers/activities.py
@@ -1,5 +1,9 @@
-from kafka import KafkaProducer
+from os.path import join, abspath, pardir, dirname
+from sys import path
+parent_dir = abspath(join(dirname(abspath(__file__)), pardir))
+path.append(parent_dir)
 from ...collectors.event.activities import get_activities
+from kafka import KafkaProducer
 
 def main():
     print(get_activities())
