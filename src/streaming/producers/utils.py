02fd6af04b0a5d2476bfe06dacda51dd62885c9a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-07 15:27:22 +0000

    Minor update

--- src/streaming/producers/utils.py
+++ src/streaming/producers/utils.py
@@ -6,6 +6,7 @@ parent_dir = abspath(join(dirname(abspath(__file__)), pardir))
 path.append(parent_dir)
 from common.utils import *
 from kafka import KafkaProducer
+import pandas as pd
 
 def get_parent(par_dir):
     prefix = join(abspath(join(parent_dir, pardir)), "data") 
@@ -18,5 +19,8 @@ def send_list_data_as_stream(records: list, server: KafkaProducer, stream_name: 
         server.send(stream_name, value=record)
 
 def send_data_as_stream(records, server: KafkaProducer, stream_name: str) -> None:
+    if isinstance(records, list):
+        send_list_data_as_stream(records, server, stream_name)
+    elif isinstance(records, pd.DataFrame)
     for record in records:
         server.send(stream_name, value=record)
