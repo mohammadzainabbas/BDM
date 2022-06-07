d83e493fd3ddbddb43032c5998c7d593837046fe
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-07 13:34:16 +0000

    Minor changes

--- scripts/create_kafka_topic.sh
+++ scripts/create_kafka_topic.sh
@@ -12,6 +12,27 @@ set -e -u pipefail
 # import helper functions from 'scripts/utils.sh'
 . $(dirname $0)/utils.sh
 
+#Function that shows usage for this script
+function usage()
+{
+cat << HEREDOC
+Create Kafka topic/stream
+Usage: 
+    
+    $progname [OPTION] [Value]
+Options:
+    -t, --topic             Name of the topic (aka stream)
+    -p, --path              Path for tpcds directory. (by default uses '../tpcds-kit')
+    -h, --help              Show usage
+Examples:
+    $ $progname -s 1
+    ⚐ → Benchmark for 1 Gb scale.
+    $ $progname -s 5 -p ../tpcds-kit/
+    ⚐ → Benchmark for 5 Gb scale with tpcds dir path as '../tpcds-kit'.
+HEREDOC
+}
+
+
 KAFKA_DIR=~/BDM_Software/kafka_2.13-3.1.0
 topic=$1
 
