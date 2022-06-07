fc9ff0d7dd60ae0816afb8f8620b98e1db60c103
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-07 16:45:29 +0000

    Minor update

--- src/streaming/producers/activities.py
+++ src/streaming/producers/activities.py
@@ -50,22 +50,11 @@ def get_activities(server: KafkaProducer, stream_name: str):
 
 def main():
     
-    # config = get_kafka_producer_config() # Get all configurations for Kafka producer
-    # stream_name = get_kafka_topic() # name of the stream
+    config = get_kafka_producer_config() # Get all configurations for Kafka producer
+    stream_name = get_kafka_topic() # name of the stream
     
-    # server = KafkaProducer(**config)
-    # get_activities(server)
-    get_total()
-
-    # print(get_kafka_config())
-
-    # activity_type = "activities"
-    # today_date = get_today_date()
-    # path = get_parent(join(today_date, activity_type))
-    # data = get_activities()
-    # file_path = join(path, '{}_{}.csv'.format(activity_type, today_date))
-    # json_to_csv(data, file_path)
-    # print_log("Fetched {} records on {} for '{}' from base url '{}' and saved to '{}'".format(len(data), today_date, activity_type, BASE_URL, file_path))
+    server = KafkaProducer(**config)
+    get_activities(server)
 
 if __name__ == '__main__':
     main()
