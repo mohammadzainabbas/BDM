b37cdb332fd144fb78ff3aeb4a7d696326404fd9
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-07 14:04:50 +0000

    Updated code

--- src/streaming/producers/activities.py
+++ src/streaming/producers/activities.py
@@ -2,12 +2,12 @@ from os.path import join, abspath, pardir, dirname
 from sys import path
 parent_dir = abspath(join(join(dirname(abspath(__file__)), pardir), pardir))
 print(parent_dir)
-# path.append(parent_dir)
-# from ...collectors.event.activities import get_activities
+path.append(join(parent_dir, "collectors"))
+from collectors.event.activities import get_activities
 # from kafka import KafkaProducer
 
 def main():
-    # print(get_activities())
+    print(get_activities())
     print("GH")
 
 if __name__ == "__main__":
