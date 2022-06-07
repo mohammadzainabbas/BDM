32b3ba8ccc78e58c2ef302f5bf265b803a56b84d
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-07 13:47:31 +0000

    Minor update

---
+++ scripts/list_kafka_topics.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#====================================================================================
+# Author: Mohammad Zain Abbas
+# Date: 7th June, 2022
+#====================================================================================
+# This script is used to list all kafka topic/streams on ubuntu. 
+#====================================================================================
+
+# Enable exit on error
+set -e -u
+
+# import helper functions from 'scripts/utils.sh'
+. $(dirname $0)/utils.sh
+
+KAFKA_DIR=~/BDM_Software/kafka_2.13-3.1.0
+
+log "Listing Kafka topic/stream"
+
+sh $KAFKA_DIR/bin/kafka-topics.sh --list --bootstrap-server localhost:9092
+
+log "All done !!"
+
+# bash scripts/create_kafka_topic.sh -t bcn_events
