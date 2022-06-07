5e645b9277b0f38825c0f4f5b39e13ebf34f6141
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-07 15:42:40 +0000

    Work in progress

--- src/streaming/producers/activities.py
+++ src/streaming/producers/activities.py
@@ -10,6 +10,7 @@ START_URL = "/api/action/datastore_search?resource_id=877ccf66-9106-4ae2-be51-95
 def get_total() -> int:
     _result_ = defaultdict(lambda: None, fetch_data("{}{}".format(BASE_URL, START_URL), verbose=False) )
     _total_ = _result_['result']['total']
+    print( _result_['result']['records'][:3] )
     print( type(_result_['result']['records']) )
     return int(_total_ if _total_ else 0)
 
