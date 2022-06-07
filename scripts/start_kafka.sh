36ef4ce69b1e5ecc2dd7868767cfea4b51bce0e0
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-07 13:23:05 +0000

    Work in progress

--- scripts/start_kafka.sh
+++ scripts/start_kafka.sh
@@ -6,3 +6,9 @@
 # This script is used to start Kafka server on ubuntu. 
 #====================================================================================
 
+# Enable exit on error
+set -e -u pipefail
+
+# import helper functions from 'scripts/utils.sh'
+. $(dirname $0)/utils.sh
+
