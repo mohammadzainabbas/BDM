9b3bd41ee7952f155d6c3819f3932c667483184b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-07 14:09:56 +0000

    Minor update

--- src/streaming/producers/utils.py
+++ src/streaming/producers/utils.py
@@ -3,3 +3,9 @@ from sys import path
 parent_dir = abspath(join(dirname(abspath(__file__)), pardir))
 path.append(parent_dir)
 from common.utils import *
+
+def get_parent(par_dir):
+    prefix = join(abspath(join(parent_dir, pardir)), "data") 
+    path = join(prefix, "events", par_dir)
+    create_if_not_exists(path)
+    return path
