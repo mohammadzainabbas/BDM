4bc9794f231b90191aa2230386ea5fdd6e5d6fd4
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-07 14:05:51 +0000

    Minor update

--- src/streaming/producers/activities.py
+++ src/streaming/producers/activities.py
@@ -3,7 +3,7 @@ from sys import path
 parent_dir = abspath(join(join(dirname(abspath(__file__)), pardir), pardir))
 print(parent_dir)
 path.append(join(parent_dir, "collectors"))
-from collectors.event.activities import get_activities
+from event.activities import get_activities
 # from kafka import KafkaProducer
 
 def main():
