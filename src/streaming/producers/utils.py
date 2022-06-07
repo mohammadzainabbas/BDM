34558b5f004051115c8213f138d682de9a6e4cdf
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-07 14:08:55 +0000

    Minor update

---
+++ src/streaming/producers/utils.py
@@ -0,0 +1,5 @@
+from os.path import join, abspath, pardir, dirname
+from sys import path
+parent_dir = abspath(join(dirname(abspath(__file__)), pardir))
+path.append(parent_dir)
+from common.utils import *
