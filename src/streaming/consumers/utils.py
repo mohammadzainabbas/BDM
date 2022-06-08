c78b2673bfe9b590060987b2a19607844688b721
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-08 12:00:40 +0000

    Minor change

--- src/streaming/consumers/utils.py
+++ src/streaming/consumers/utils.py
@@ -4,3 +4,12 @@ parent_dir = abspath(join(dirname(abspath(__file__)), pardir))
 path.append(parent_dir)
 from common.utils import *
 
+from traitlets import Any
+
+# Helper methods for Kafka consumer
+
+def store_streaming_data_in_hdfs(data: Any, hdfs_location: str, format: str = 'parquet') -> None:
+    
+
+
+
