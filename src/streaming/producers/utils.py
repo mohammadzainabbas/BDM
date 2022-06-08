4f318b053c88e750dbc11913222813d8951170fa
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-08 23:16:34 +0000

    Minor changes

--- src/streaming/producers/utils.py
+++ src/streaming/producers/utils.py
@@ -18,7 +18,8 @@ def parse_record(record: dict) -> dict:
     Reference: https://stackoverflow.com/a/71167976/6390175
     """
     __record, __keys = dict(), record.keys()
-    for __key in __keys: __record[__key] = safe_load( record[__key] )
+    for __key in __keys:
+        __record[__key] = safe_load( record[__key] )
     return __record
 
 # Helper methods for Kafka Producer
