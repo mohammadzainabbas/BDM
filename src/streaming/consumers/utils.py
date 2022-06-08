2fb9055f0b42c259fd24a9ebcbf9bc64d2cc05aa
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-08 12:02:43 +0000

    Work in progress

--- src/streaming/consumers/utils.py
+++ src/streaming/consumers/utils.py
@@ -3,15 +3,17 @@ from sys import path
 parent_dir = abspath(join(dirname(abspath(__file__)), pardir))
 path.append(parent_dir)
 from common.utils import *
-
 from traitlets import Any
-
+import pandas as pd
 
 # Helper methods for Kafka consumer
 
-def store_list_data_in_hdfs(data: list, hdfs_location: str, format: str = 'parquet') -> None:
+def store_dataframe_data_in_hdfs(df: pd.DataFrame, hdfs_location: str, format: str = 'parquet') -> None:
 
-def store_dataframe_data_in_hdfs(data: list, hdfs_location: str, format: str = 'parquet') -> None:
+def store_list_data_in_hdfs(data: list, hdfs_location: str, format: str = 'parquet') -> None:
+    df = pd.DataFrame(data)
+    store_dataframe_data_in_hdfs(df, hdfs_location, format)
+
 
 
 def store_streaming_data_in_hdfs(data: Any, hdfs_location: str, format: str = 'parquet') -> None:
