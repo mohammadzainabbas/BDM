b75a8213e4cbb40061cfae3cacc05b7dd98f5173
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-08 12:39:20 +0000

    Updated code

--- src/streaming/consumers/utils.py
+++ src/streaming/consumers/utils.py
@@ -17,6 +17,9 @@ from hdfs.util import HdfsError
 def get_spark_session() -> session.SparkSession:
     return SparkSession.builder.appName("bdm").getOrCreate()
 
+def get_streaming_spark_session() -> session.SparkSession:
+    return S
+
 def save_df_as_parquet(file_path: str, df: pd.DataFrame) -> None:
     spark = get_spark_session()
     spark.createDataFrame(df).write.parquet(file_path)
