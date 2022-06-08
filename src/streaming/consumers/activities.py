7deea08f9118b6bb2bc265e3454f65a29fa7f918
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-08 12:13:55 +0000

    Minor update

--- src/streaming/consumers/activities.py
+++ src/streaming/consumers/activities.py
@@ -1,9 +1,11 @@
+import imp
 from os.path import join
 from json import dumps, loads
 from streaming.common.utils import print_error
 from utils import get_today_date, get_parent, store_streaming_data_in_hdfs, print_log, get_kafka_consumer_config, get_kafka_topic
 from collections import defaultdict
 from kafka import KafkaConsumer
+from inspect import stack
 
 def get_activities_from_stream(consumer: KafkaConsumer) -> None:
     __push_after_items = 10000 # No. of messages after which you have to store the data in HDFS  
