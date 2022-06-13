8b438b287ed3a441389b4f36643ada7bfdaf73f2
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-13 08:32:19 +0000

    Minor change

--- src/formatters/event/fp_growth.py
+++ src/formatters/event/fp_growth.py
@@ -30,6 +30,10 @@ def get_random_users_list(users_count = 100):
     return [get_random_name() for i in range(0, users_count)]
 
 def main():
+    """
+    Since, we don't have the users data, so, we decided to generate the users data and then build an FP growth model to get most frequent sets.
+    This will help us in the future for recommending users to visit new places.
+    """
 
     #=======================
     # Configurations
