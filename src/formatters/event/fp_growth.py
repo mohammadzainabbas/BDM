cd08521d34dea49a558f68b5442085ccd2fc6160
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-13 07:24:53 +0000

    Minor

--- src/formatters/event/fp_growth.py
+++ src/formatters/event/fp_growth.py
@@ -77,15 +77,8 @@ def main():
     cols = ["user", "type", "name"]
     df = df.select(cols)
 
-    activity_type = "activities"
-    files, prefix = get_files(activity_type)
-    print_log("Loading {} files of '{}' into HDFS".format(len(files), activity_type))
-    hdfs_path = "{}/{}/{}".format(get_hdfs_user_home(), prefix, activity_type)
-    # for file in files:
-    #     file_name = str(file).split("/")[-1]
-    #     print_log("File '{}' moved to HDFS at '{}'".format(file, hdfs_path))
-    #     write_to_hdfs(join(hdfs_path, file_name), file)
-    print_log("Saved {} file(s) of '{}' into HDFS at '{}'".format(len(files), activity_type, hdfs_path))
+    
+
 
 if __name__ == '__main__':
     main()
