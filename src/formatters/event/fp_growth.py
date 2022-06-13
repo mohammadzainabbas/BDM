097ad39238e31b029a94f963148b80c2480940a4
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-06-13 07:10:25 +0000

    Updated code

--- src/formatters/event/fp_growth.py
+++ src/formatters/event/fp_growth.py
@@ -3,10 +3,7 @@ from pyspark.sql import SparkSession, SQLContext, functions as SF
 from pyspark import SparkContext, SQLContext
 import names
 from functools import reduce
-from hdfs import InsecureClient
-from utils import get_hdfs_user_home, get_files, write_to_hdfs, print_log
-
-
+from utils import get_hdfs_client, get_files, write_to_hdfs, print_log
 
 activities_cols = ['register_id', 'name', 'geo_epgs_4326_x', 'geo_epgs_4326_y', # Must
     'addresses_neighborhood_id', 'addresses_neighborhood_name', # For neighborhood's query
